4e7d11f58b6545dddf919ee98d2962a491eaf0f8
k05t1k <isip_k.m.fionov@mpt.ru>
2024-03-22 16:54:25 +0300

    first commit

---
+++ dataset/dataset/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using dataset.PizzeriaDataSetTableAdapters;
+
+namespace dataset
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PizzaTableAdapter pizza = new PizzaTableAdapter();
+        ClientTableAdapter client = new ClientTableAdapter();
+        PersonalTableAdapter personal = new PersonalTableAdapter();
+        OrdersTableAdapter orders = new OrdersTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            FilterCombo.ItemsSource = personal.GetData();
+            FilterCombo.DisplayMemberPath = "FirstName";
+        }
+        public int table = 0;
+        public void SwitcherOutput()
+        {
+            switch (table)
+            {
+                case 0:
+                    PizzaDataGrid.ItemsSource = pizza.GetData();
+                    SearchBox.Text = "Название пиццы";
+                    break;
+                case 1:
+                    PizzaDataGrid.ItemsSource = client.GetData();
+                    SearchBox.Text = "Имя клиента";
+                    break;
+                case 2:
+                    PizzaDataGrid.ItemsSource = personal.GetData();
+                    SearchBox.Text = "Имя персонала";
+                    break;
+                case 3:
+                    PizzaDataGrid.ItemsSource = orders.GetData();
+                    SearchBox.Text = "Цена заказа";
+                    break;
+
+            }
+        }
+        public void SwitcherSearch()
+        {
+            switch (table)
+            {
+                case 0:
+                    PizzaDataGrid.ItemsSource = pizza.GetDataBy(SearchBox.Text);
+                    break;
+                case 1:
+                    PizzaDataGrid.ItemsSource = client.GetDataBy(SearchBox.Text);
+                    break;
+                case 2:
+                    PizzaDataGrid.ItemsSource = personal.GetDataBy(SearchBox.Text);
+                    break;
+                case 3:
+                    PizzaDataGrid.ItemsSource = orders.GetDataBy(decimal.Parse(SearchBox.Text));
+                    break;
+
+            }
+        }
+        public void Filter()
+        {
+            if (FilterCombo.SelectedItem != null)
+            {
+                var id = (int)(FilterCombo.SelectedItem as DataRowView).Row[0];
+                PizzaDataGrid.ItemsSource = orders.FilterData(id);
+            }
+        }
+        private void NextButton(object sender, RoutedEventArgs e)
+        {
+            if (table == 3)
+                table -= 4;
+                
+            table++;
+            SwitcherOutput();
+        }
+        private void PreviousButton(object sender, RoutedEventArgs e)
+        {
+            if (table == 0)
+                table += 4;
+
+            table--;
+            SwitcherOutput();
+        }
+        private void SearchButton(object sender, RoutedEventArgs e)
+        {
+            SwitcherSearch();
+        }
+
+        private void FilterCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+        private void FilterButton(object sender, RoutedEventArgs e)
+        {
+            PizzaDataGrid.ItemsSource = orders.GetData();
+            SearchBox.Text = "Цена заказа";
+        }
+    }
+}
---
+++ entityframe/entityframe/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace entityframe
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FilterCombo.ItemsSource = context.Personals.ToList();
+            FilterCombo.DisplayMemberPath = "FirstName";
+        }
+
+        private PizzeriaEntities context = new PizzeriaEntities();
+
+        public int table = 0;
+
+        public void SwitcherOutput()
+        {
+            switch (table)
+            {
+                case 0:
+                    PizzaDataGrid.ItemsSource = context.Pizzas.ToList();
+                    SearchBox.Text = "Название пиццы";
+                    break;
+                case 1:
+                    PizzaDataGrid.ItemsSource = context.Clients.ToList();
+                    SearchBox.Text = "Имя клиента";
+                    break;
+                case 2:
+                    PizzaDataGrid.ItemsSource = context.Personals.ToList();
+                    SearchBox.Text = "Имя персонала";
+                    break;
+                case 3:
+                    PizzaDataGrid.ItemsSource = context.Orders.ToList();
+                    SearchBox.Text = "Цена заказа";
+                    break;
+
+            }
+        }
+
+        public void SwitcherSearch()
+        {
+            switch (table)
+            {
+                case 0:
+                    PizzaDataGrid.ItemsSource = context.Pizzas.ToList().Where(item => item.PizzaName.Contains(SearchBox.Text));
+                    break;
+                case 1:
+                    PizzaDataGrid.ItemsSource = context.Clients.ToList().Where(item => item.FirstName.Contains(SearchBox.Text));
+                    break;
+                case 2:
+                    PizzaDataGrid.ItemsSource = context.Personals.ToList().Where(item => item.FirstName.Contains(SearchBox.Text));
+                    break;
+                case 3:
+                    PizzaDataGrid.ItemsSource = context.Orders.ToList().Where(item => item.Price.ToString().Contains(SearchBox.Text));
+                    break;
+
+            }
+        }
+
+        private void NextButton(object sender, RoutedEventArgs e)
+        {
+            if (table == 3)
+                table -= 4;
+
+            table++;
+            SwitcherOutput();
+        }
+
+        private void PreviousButton(object sender, RoutedEventArgs e)
+        {
+            if (table == 0)
+                table += 4;
+
+            table--;
+            SwitcherOutput();
+        }
+
+        private void SearchButton(object sender, RoutedEventArgs e)
+        {
+            SwitcherSearch();
+        }
+
+        private void FilterButton(object sender, RoutedEventArgs e)
+        {
+            PizzaDataGrid.ItemsSource = context.Orders.ToList();
+            SearchBox.Text = "Цена заказа";
+        }
+
+        private void FilterCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (FilterCombo.SelectedItem != null)
+            {
+                var selected = FilterCombo.SelectedItem as Personal;
+                PizzaDataGrid.ItemsSource = context.Orders.ToList().Where(item => item.Personal == selected);
+            }
+        }
+    }
+}
